534961e79b4c513d99e1ba03c93a8f92546a05d4
Ben Rehmann <brehmann@me.com>
2024-05-01 10:55:53 -0500

    build folder included now

--- src/Pages/CustomWork/CustomWork.jsx
+++ src/Pages/CustomWork/CustomWork.jsx
@@ -123,6 +123,7 @@ const CustomWork = () => {
                             exerciseList={exerciseList}
                             setExerciseList={setExerciseList}
                         />
+                        
                     ))}
                 </div>
             </div>
